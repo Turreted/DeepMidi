d1cd40db4bdd63c522f318726715669d76ddf3e7
Turreted <gideonmitchell01@gmail.com>
2020-06-17 08:08:57 -0400

    Add process to download dataset in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,13 @@
 git clone https://github.com/rogersce/cnpy.git ./encoding/libs/cnpy
 git clone https://github.com/craigsapp/midifile.git ./encoding/libs/midifile
 
+# Download dataset
+cd dataset
+wget https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip
+unzip maestro-v2.0.0-midi.zip
+rm maestro-v2.0.0-midi.zip
+cd ..
+
 # compile cpp binaries
 mkdir ./encoding/build
 cd ./encoding/build
