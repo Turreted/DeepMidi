4ecfa6c8f3d8b8562d027a6cd0ad8c19438d39a6
Turreted <gideonmitchell01@gmail.com>
2020-06-18 04:41:46 -0400

    Fix dataset directory bug

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git clone https://github.com/rogersce/cnpy.git ./encoding/libs/cnpy
 git clone https://github.com/craigsapp/midifile.git ./encoding/libs/midifile
 
 # Download dataset
+mkdir dataset
 cd dataset
 wget https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip
 unzip maestro-v2.0.0-midi.zip
